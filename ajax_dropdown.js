bb6b3a3e0dd4c6e098ff21619da61e359ea30aa1
Jan Varwig <jan@varwig.org>
2009-11-12 12:21:32 +0100

    Whitespace fixes

--- ajax_dropdown.js
+++ ajax_dropdown.js
@@ -1,12 +1,12 @@
 /*
- * 	<%= f.text_field :leiter_id,
-                     :class => "ajax_dropdown",
-                     :datasrc => find_json_people_path,
-                     :original_content => f.object.leiter.description,
-                     :callback => "some_js_callback_code();" %>       Callback Code is executed in the context of the input field whenever the value changes
+ *	<%= f.text_field :leiter_id,
+										 :class => "ajax_dropdown",
+										 :datasrc => find_json_people_path,
+										 :original_content => f.object.leiter.description,
+										 :callback => "some_js_callback_code();" %>				Callback Code is executed in the context of the input field whenever the value changes
  */
 var AJAXDropdown = (function(){
-	
+
 	/* Create the hidden field that holds the ID */
 	function make_hidden_field(inputfield) {
 		inputfield.after('<input type="hidden" readonly="true" name="' + inputfield.attr("name") + '" value="' + inputfield.val() +'"/>')
@@ -14,30 +14,30 @@ var AJAXDropdown = (function(){
 		inputfield.removeAttr("name")
 		return inputfield.next()[0]
 	}
-	
+
 	/* Create the Dropdownlost as a hidden layer, install handlers */
 	function make_ajax_list(inputfield) {
 		inputfield.after("<div class=\"ajax_dropdown_div\"><span></span><table></table></div>")
 		var dj = inputfield.next()
-		var d  = dj[0]
-		
+		var d	 = dj[0]
+
 		//Style
 		dj.css("position", "absolute")
 		var offset = inputfield.offset()
 		dj.css("left", offset.left)
 		dj.css("top", offset.top+inputfield.height()+30)
 		dj.css("display", "none")
-		
+
 		//Handlers
 		$("table", d).mousedown(function(){
 			d.pass_selected_value()
 			if (inputfield[0].callback) inputfield[0].callback.apply(inputfield[0])
 		})
-		
+
 		//Functions
-		
+
 		//Move selected up
-		d.up   = function(){
+		d.up	 = function(){
 			var current = $(".selected", dj)
 			if (current.length == 0) {
 				$("tr:last", dj).addClass("selected")
@@ -49,7 +49,7 @@ var AJAXDropdown = (function(){
 					current.siblings(":last").addClass("selected")
 			}
 		}
-		
+
 		//Move selected down
 		d.down = function(){
 			var current = $(".selected", dj)
@@ -63,11 +63,11 @@ var AJAXDropdown = (function(){
 					current.siblings(":first").addClass("selected")
 			}
 		}
-		
+
 		d.deselect_all = function() {
 			$("tr", dj).removeClass("selected")
 		}
-		
+
 		//Hide and Show
 		d.hide = function(){ dj.hide() }
 		d.show = function(){
@@ -76,7 +76,7 @@ var AJAXDropdown = (function(){
 			dj.css("top", offset.top+inputfield.height()+ 5)
 			dj.show()
 		}
-		
+
 		//Pass the currently selected Value to the Inputfield
 		d.pass_selected_value = function() {
 			var s = $(".selected", d)
@@ -86,12 +86,12 @@ var AJAXDropdown = (function(){
 				inputfield[0].update_from_list('','')
 			}
 		}
-		
+
 		//Set a message to appear on top of the list
 		d.set_message = function(msg){
 			$("span", dj).html(msg)
 		}
-		
+
 		//Callback, executed when the query has changed
 		d.query_changed = function(querystring){
 			querystring = querystring.split(/,? +|,/)
@@ -103,7 +103,7 @@ var AJAXDropdown = (function(){
 					d.results = results
 					if (results.length > 0) {
 						if (results.length > 20) d.set_message("Bitte genauer")
-						else                     d.set_message("")
+						else										 d.set_message("")
 					} else {
 						d.set_message("Nichts gefunden")
 					}
@@ -115,7 +115,7 @@ var AJAXDropdown = (function(){
 			// var r = new RegExp(querystring, "i")
 			// var highlighted_result = results[i][1].replace(r, '<strong>$&</strong>')
 		}
-		
+
 		d.filter_results = function(filter) {
 			var table = $("table", dj)
 			table.empty()
@@ -126,7 +126,7 @@ var AJAXDropdown = (function(){
 				var rb = new RegExp(filter.join('|'), "ig")
 			} else {
 				var r2 = null
-				var rb = new RegExp(filter[0], "ig")				
+				var rb = new RegExp(filter[0], "ig")
 			}
 
 			//create table rows
@@ -138,7 +138,7 @@ var AJAXDropdown = (function(){
 							table.append('<tr dropdown_id="' + this.results[i][0] + '" dropdown_val="' + this.results[i][1] + '"><td>' + this.results[i][1].replace(rb, '<strong>$&</strong>') + '</td></tr>')
 						} else {
 							table.append('<tr dropdown_id="' + this.results[i][0] + '" dropdown_val="' + this.results[i][1] + '"><td>' + this.results[i][1].replace(rb, '<strong>$&</strong>') + '</td><td>' + this.results[i][2].replace(rb, '<strong>$&</strong>') + '</td></tr>')
-						}						
+						}
 					}
 				}
 			}
@@ -150,7 +150,7 @@ var AJAXDropdown = (function(){
 				d.deselect_all()
 			})
 		}
-		
+
 		return d
 	}
 
@@ -182,25 +182,25 @@ var AJAXDropdown = (function(){
 			}
 		}
 	}
-	
+
 	//Given a block of code in a string,
 	//returns a function that implements that code and returns true
 	function extract_callback(code){
-	  if (code == undefined) {
-	    return undefined
-	  } else {
-	    return eval("(function(){try{"+code+"}catch(e){alert('Exception in Callback!' + e.toString())};})")
-	  }
+		if (code == undefined) {
+			return undefined
+		} else {
+			return eval("(function(){try{"+code+"}catch(e){alert('Exception in Callback!' + e.toString())};})")
+		}
 	}
-	
+
 	function activate_input_field(){
 		var inpf = $(this)
 		//Input Feld aufbohren
-		this.datasource  = new Datasource(inpf.attr("datasrc"))
-		this.list        = make_ajax_list(inpf)
+		this.datasource	 = new Datasource(inpf.attr("datasrc"))
+		this.list				 = make_ajax_list(inpf)
 		this.hiddenfield = make_hidden_field(inpf)
-		this.callback    = extract_callback(inpf.attr('callback'))
-		
+		this.callback		 = extract_callback(inpf.attr('callback'))
+
 		this.update_from_list = function(id, value) {
 			//TODO: Das muss anders gehen wenn es sich um ein multivalue Feld handelt
 			this.value = value
@@ -208,11 +208,11 @@ var AJAXDropdown = (function(){
 			this.hiddenfield.value = id
 			this.list.hide();
 		}
-		
+
 		// Keypress handling
 		this.oldval = this.value
-		inpf.keypress(function(e) {     //Navigation and Submitting
-			if (e.keyCode == 38)        { //up
+		inpf.keypress(function(e) {			//Navigation and Submitting
+			if (e.keyCode == 38)				{ //up
 				this.list.up()
 				e.preventDefault()
 			} else if (e.keyCode == 40) { //down
@@ -220,7 +220,7 @@ var AJAXDropdown = (function(){
 				e.preventDefault()
 			} else if (e.keyCode == 13) { //enter
 				if ($(this.list).css("display") == "none") {
-				  return true
+					return true
 				} else {
 					this.list.pass_selected_value()
 					if (this.callback) this.callback.apply(this)
@@ -229,7 +229,7 @@ var AJAXDropdown = (function(){
 				}
 			}
 		})
-		inpf.keyup(function(e) {        //Entering letters
+		inpf.keyup(function(e) {				//Entering letters
 			if(e.keyCode != 13) //enter
 			if(this.value != this.oldval) { //only operate on changes
 				this.oldval = this.value
@@ -249,7 +249,7 @@ var AJAXDropdown = (function(){
 			this.list.show();
 		})
 		inpf.attr("autocomplete", "off")
-		
+
 	}
 	return { /* PUBLIC */
 		activate_all : function(){ $(".ajax_dropdown").each(activate_input_field) }
